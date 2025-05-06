d8271ff64413e15d8cf607cfb69f8096482181f1
NASUTONG <masutong6@gmail.com>
2025-05-06 14:55:26 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,40 @@
+let cubi = [];
+let copie = 30;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, "webgl");
+
+  for (let i = 0; i < copie; i++) {
+    let cubo = {
+      x: random(-1000, 1000),
+      y: random(-1000, 1000),
+      z: random(-1000, 1000),
+      size: 100,
+      color: random(["pink", "yellow", "blue"]),
+      rotationAxis: random(["x", "y", "z"]),
+    };
+    cubi.push(cubo);
+  }
 }
 
 function draw() {
   background(220);
-  const s = 100;
+  orbitControl();
 
-  rect(0, 0, s, s);
-  rect(0, height - s, s, s);
-  rect(width - s, 0, s, s);
-  rect(width - s, height - s, s, s);
+  for (let cubo of cubi) {
+    push();
+    translate(cubo.x, cubo.y, cubo.z);
+    if (cubo.rotationAxis == "x") {
+      rotateX(frameCount / 10);
+    } else if (cubo.rotationAxis == "y") {
+      rotateY(frameCount / 10);
+    } else {
+      rotateZ(frameCount / 10);
+    }
+    fill(cubo.color);
+    box(cubo.size);
+    pop();
+  }
 }
 
 function windowResized() {
